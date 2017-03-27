6410b513afb08c4e3fbee0c1dc9e66fd4bae849e
franksco <franksco39@gmail.com>
2017-03-27 15:18:45 -0700

    adds label, NSTimer, and updateTimer func that gives us the correct time in a (medium format)

--- DigiClock/ViewController.swift
+++ DigiClock/ViewController.swift
@@ -9,15 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var label: UILabel!
+    
+    var timer = Timer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(ViewController.updateTimer), userInfo: nil, repeats: true)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func updateTimer() {
+        
+        let timerFormatter = DateFormatter()
+        
+        timerFormatter.timeStyle = .medium
+        
+        label.text = timerFormatter.string(from: Date())
+        
     }
 
 
