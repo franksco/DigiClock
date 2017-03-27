0fb146d6dcabc99ef5042e2041daf3fa4d035311
franksco <franksco39@gmail.com>
2017-03-27 16:36:20 -0700

    adds IBO's and IBA's for textColor Seg, backgroundColorSeg, and settingsButton

--- DigiClock/ViewController.swift
+++ DigiClock/ViewController.swift
@@ -14,6 +14,11 @@ class ViewController: UIViewController {
     
     var timer = Timer()
 
+    @IBOutlet var settingsView: UIView!
+    @IBOutlet var textColorSeg: UISegmentedControl!
+    @IBOutlet var backgroundColorSeg: UISegmentedControl!
+    @IBOutlet var settingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -33,6 +38,24 @@ class ViewController: UIViewController {
         timerFormatter.timeStyle = .medium
         
         label.text = timerFormatter.string(from: Date())
+        
+    }
+    
+    @IBAction func settings(_ sender: Any) {
+        
+        
+        
+    }
+    
+    @IBAction func textColor(_ sender: Any) {
+        
+        
+        
+    }
+    
+    @IBAction func backgroundColor(_ sender: Any) {
+        
+        
         
     }
 
